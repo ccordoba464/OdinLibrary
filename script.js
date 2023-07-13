7a8b98997140a05ccd9e650eac4a602d6842f4d1
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-07-13 18:23:52 -0400

    Add Form method

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ class library {
     this.myLibrary = [];
     this.bookContainer = document.getElementById("book-container");
     this.addButton = document.getElementById("addButton");
+    this.addForm();
   }
 
   addBookToLibrary(book) {
@@ -194,142 +195,7 @@ let firstBook = new Book("Music to my ears", "Rimbo", 23, true);
 let secondBook = new Book("Music to my nose", "Rimbo", 23, true);
 let thirdBook = new Book("Music to my eyes", "Rimbo", 23, true);
 
-myLibrary.addForm();
 myLibrary.addBookToLibrary(firstBook);
 myLibrary.addBookToLibrary(secondBook);
 myLibrary.addBookToLibrary(thirdBook);
 myLibrary.displayBooks();
-
-// addBookToLibrary(firstBook);
-// addBookToLibrary(secondBook);
-// addBookToLibrary(thirdBook);
-// displayBooks();
-
-// addBookToLibrary(book) {
-//   myLibrary.push(book);
-// }
-
-// function displayBooks() {
-//   while (bookContainer.firstChild) {
-//     bookContainer.removeChild(bookContainer.firstChild);
-//   }
-
-//   for (let i = 0; i < myLibrary.length; i++) {
-//     myLibrary[i].index = i;
-//     let aBook = document.createElement("div");
-//     aBook.classList.add("book");
-
-//     let desiredProps = ["title", "author", "numPages", "readBook"];
-
-//     for (let prop in myLibrary[i]) {
-//       if (desiredProps.includes(prop)) {
-//         let aProp = document.createElement("div");
-//         aProp.classList.add("prop");
-//         aProp.textContent = myLibrary[i][prop];
-//         aBook.appendChild(aProp);
-//       }
-//     }
-
-//     let removeButton = document.createElement("button");
-//     removeButton.textContent = "Remove";
-//     aBook.appendChild(removeButton);
-
-//     removeButton.addEventListener("click", () => {
-//       removeBook(myLibrary[i].index);
-//     });
-
-//     let readButton = document.createElement("button");
-//     readButton.textContent = "Read";
-//     aBook.appendChild(readButton);
-
-//     readButton.addEventListener("click", () => {
-//       myLibrary[i].readBook = !myLibrary[i].readBook;
-//       displayBooks();
-//     });
-
-//     bookContainer.appendChild(aBook);
-//   }
-// }
-
-// function removeBook(bookIndex) {
-//   console.log(bookIndex);
-//   myLibrary.splice(bookIndex, 1);
-//   displayBooks();
-// }
-
-// addButton.addEventListener("click", () => {
-//   let form = document.createElement("form");
-//   let input1 = document.createElement("input");
-//   let label1 = document.createElement("label");
-//   let input2 = document.createElement("input");
-//   let label2 = document.createElement("label");
-//   let input3 = document.createElement("input");
-//   let label3 = document.createElement("label");
-//   let input4 = document.createElement("input");
-//   let label4 = document.createElement("label");
-//   let submitButton = document.createElement("button");
-
-//   label1.for = "title";
-//   label1.textContent = "Title:";
-
-//   input1.type = "text";
-//   input1.name = "title";
-//   input1.id = "title";
-//   input1.placeholder = "Title";
-
-//   label2.for = "author";
-//   label2.textContent = "Author:";
-
-//   input2.type = "text";
-//   input2.name = "author";
-//   input2.id = "author";
-//   input2.placeholder = "Author";
-
-//   label3.for = "numPages";
-//   label3.textContent = "# of pages:";
-
-//   input3.type = "text";
-//   input3.name = "numPages";
-//   input3.id = "numPages";
-//   input3.placeholder = "Number of Pages";
-
-//   label4.for = "readBook";
-//   label4.textContent = "Read?";
-
-//   input4.type = "checkbox";
-//   input4.name = "readBook";
-//   input4.id = "readBook";
-
-//   submitButton.type = submitButton;
-//   submitButton.textContent = "Submit Book";
-//   submitButton.style.gridColumn = "1 / 3";
-//   submitButton.id = submitButton;
-
-//   form.appendChild(label1);
-//   form.appendChild(input1);
-//   form.appendChild(label2);
-//   form.appendChild(input2);
-//   form.appendChild(label3);
-//   form.appendChild(input3);
-//   form.appendChild(label4);
-//   form.appendChild(input4);
-//   form.appendChild(submitButton);
-
-//   bookContainer.appendChild(form);
-
-//   submitButton.addEventListener("click", () => {
-//     event.preventDefault();
-
-//     let titleInput = document.getElementById("title").value;
-//     let authorInput = document.getElementById("author").value;
-//     let pagesInput = document.getElementById("numPages").value;
-//     let readInput = document.getElementById("readBook").checked;
-
-//     let newBook = new Book(titleInput, authorInput, pagesInput, readInput);
-
-//     addBookToLibrary(newBook);
-
-//     console.log(newBook.index);
-//     displayBooks();
-//   });
-// });
